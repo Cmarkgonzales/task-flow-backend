cab9ef6409bc9d8b595ad4cbce1e4a3deaae4687
Chris Chan <cmarkgonzales.bposeats@gmail.com>
2025-06-10 03:55:01 +0800

    Add railway configuration

--- src/main/java/com/task_flow/tracker/TrackerApplication.java
+++ src/main/java/com/task_flow/tracker/TrackerApplication.java
@@ -2,7 +2,6 @@ package com.task_flow.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TrackerApplication {
