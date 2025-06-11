12a15ff1f253704715517461b8a7d52e7ab1cdea
Chris Chan <cmarkgonzales.17@gmail.com>
2025-06-12 02:41:07 +0800

    Add home controller for backend site display

---
+++ src/main/java/com/task_flow/tracker/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.task_flow.tracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Task Flow Tracker Backend is running.";
+    }
+}
